81aa25a65f65089f700b92f62d8b80a2a6aaa4ba
Fernando Glingani <fernando.glingani@gmail.com>
2021-01-20 16:38:15 -0300

    first commit

---
+++ src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { DynamicModule, Module, Global } from '@nestjs/common';
+import { MySQLProvider } from './mysql.provider';
+
+@Global()
+@Module({})
+export class DatabaseModule {
+  static forRoot(): DynamicModule {
+    const connectionFactory = {
+      provide: 'DATABASE',
+      useFactory: () => {
+        return new MySQLProvider();
+      },
+    };
+    return {
+      module: DatabaseModule,
+      providers: [connectionFactory],
+      exports: ['DATABASE'],
+    };
+  }
+}
---
+++ src/modules/product/product.resolver.ts
@@ -0,0 +1,32 @@
+import { Args, Mutation, Query } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
+import { Product } from './dto/product';
+import { ProductInput } from './dto/product.input';
+import { ProductService } from './product.service';
+
+@Resolver((of) => Product)
+export class ProductResolver {
+  constructor(private readonly productService: ProductService) {}
+  @Query((returns) => [Product], { name: 'getAllProducts' })
+  async getAllProducts(): Promise<Product[]> {
+    const products = await this.productService.findAll();
+    const productsToReturn = products.map((product) => {
+      const productToReturn = new Product();
+      productToReturn.id = product.id;
+      productToReturn.product = product.product;
+      productToReturn.price = product.price;
+      return productToReturn;
+    });
+    return productsToReturn;
+  }
+
+  @Mutation((returns) => Product, { name: 'createProduct' })
+  async create(@Args('input') input: ProductInput): Promise<Product> {
+    return this.productService.create(input);
+  }
+
+  @Mutation((returns) => Product, { name: 'removeProduct' })
+  async remove(@Args('id') id: string): Promise<boolean> {
+    return this.productService.remove(id);
+  }
+}
